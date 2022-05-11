99e120f221b7714b4ff709ba8acdedf6444a03ce
שיראל עופרי <sofri@g.jct.ac.il>
2022-05-11 09:54:12 +0300

    start part 7.2

--- src/primitives/Material.java
+++ src/primitives/Material.java
@@ -4,40 +4,35 @@ public class Material {
     public Double3 _kD = new Double3(0d);
     public Double3 _kS = new Double3( 0d);
     public int _nShininess = 0;
+    /**
+     * Reflective coefficient
+     */
     public Double3 _kr= new Double3(0d);
+    /**
+     * Transparency Coefficient
+     */
+    public Double3 _kt = new Double3(0d);
 
-    public Material setKr(Double3 kr) {
-        _kr = kr;
-        return this;
-    }
-
-    public Material setKt(Double3 kt) {
-        _kt = kt;
-        return this;
-    }
-
+    /**
+     * setter
+     * @param kr reflective coefficient
+     * @return material
+     */
     public Material setKr(double kr) {
         _kr = new Double3(kr);
         return this;
     }
 
+    /**
+     * setter
+     * @param kt transparency Coefficient
+     * @return material
+     */
     public Material setKt(double kt) {
         _kt =new Double3(kt);
         return this;
     }
 
-    public Double3 _kt = new Double3(0d);
-
-
-    public Material setKd(Double3 kD) {
-        _kD = kD;
-        return this;
-    }
-
-    public Material setKs(Double3 kS) {
-        _kS = kS;
-        return this;
-    }
 
     public Material setKd(double kD) {
         _kD = new Double3(kD);
